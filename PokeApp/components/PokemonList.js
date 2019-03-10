8e0127decbfc0b53a21640504a3bd10400999cda
James Zuckerman <jzuckerman4@gmail.com>
2019-03-09 22:42:47 -0700

    Changed layout.

--- PokeApp/components/PokemonList.js
+++ PokeApp/components/PokemonList.js
@@ -20,20 +20,66 @@ class PokemonList extends Component {
         <FlatList
           data={this.props.pokemon.filteredPokemon}
           renderItem={({ item }) => (
-            <TouchableHighlight
-              onPress={() => this.props.selectPokemon(item.id)}
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                margin: 10
+              }}
             >
-              <Text key={item.id}>
-                {item.id + '-------'}
+              <Text style={styles.pokemonNumber}>#{item.id} </Text>
+              <Text style={styles.pokemonName}>
                 {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
               </Text>
-            </TouchableHighlight>
+              <View
+                style={{
+                  height: 80,
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <View style={styles.line} />
+
+                <TouchableHighlight
+                  onPress={() => this.props.selectPokemon(item.id)}
+                >
+                  <Image
+                    source={require('../assets/pokeOutline.png')}
+                    resizeMode="stretch"
+                    style={{ marginTop: -6, height: 50, width: 50 }}
+                  />
+                </TouchableHighlight>
+              </View>
+            </View>
           )}
         />
       );
     else return null;
   }
 }
+
+const styles = StyleSheet.create({
+  pokeball: {
+    backgroundColor: 'red',
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10
+  },
+  pokemonNumber: {
+    marginTop: 55,
+    fontSize: 11,
+    color: '#bbb8b6'
+  },
+
+  pokemonName: {
+    marginTop: 55,
+    color: '#555',
+    fontWeight: '700',
+    fontSize: 11
+  },
+  line: { height: 60, width: 2, backgroundColor: '#bababb' }
+});
 
 const mapStateToProps = state => {
   return { pokemon: state.pokemon };
--- PokeApp/components/PokemonOverview.js
+++ PokeApp/components/PokemonOverview.js
@@ -5,16 +5,26 @@ import { connect } from 'react-redux';
 
 class PokemonOverview extends Component {
   render() {
-    console.log('poop');
     if (this.props.pokemon.pokemonDetail !== null)
       return (
-        <View>
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
           <Image
             style={{ width: 200, height: 200 }}
             source={{
               uri: this.props.pokemon.pokemonDetail.sprites.front_default
             }}
           />
+
+          <Text>
+            {this.props.pokemon.pokemonDetail.name.charAt(0).toUpperCase() +
+              this.props.pokemon.pokemonDetail.name.slice(1)}
+          </Text>
         </View>
       );
     else return null;
